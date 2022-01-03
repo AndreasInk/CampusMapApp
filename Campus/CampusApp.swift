6b0c55cab2be3fcfa2ab89c07c35ebaa4e9817d7
Andreas Ink <andreasink17@gmail.com>
2022-01-03 03:52:41 -0500

    Initial Commit

---
+++ Campus/CampusApp.swift
@@ -0,0 +1,17 @@
+//
+//  CampusApp.swift
+//  Campus
+//
+//  Created by Andreas Ink on 1/3/22.
+//
+
+import SwiftUI
+
+@main
+struct CampusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
